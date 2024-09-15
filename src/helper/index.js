454ee6b748e859020c6ddb74b9c4a25096c2da9e
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-16 05:41:18 +0600

    err

--- src/helper/index.js
+++ src/helper/index.js
@@ -9,61 +9,15 @@ export const getData = async (endpoint) => {
   return data;
 };
 
-export const getManSneaker = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter(
-    (item) => item.category === "Men's Sneaker"
-  );
-
-  return filterData;
+const getFilteredData = async (category) => {
+  const products = await getData("http://localhost:3000/api/allproducts");
+  return products.allData.filter((item) => item.category === category);
 };
-export const getMenPant = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Men's Pants");
 
-  return filterData;
-};
-export const getManBoot = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Men's Boot");
-
-  return filterData;
-};
-export const getManBag = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Bag");
-
-  return filterData;
-};
-export const getManCap = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Cap");
-
-  return filterData;
-};
-export const getManEarPhone = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Earphones");
-
-  return filterData;
-};
-export const getBottle = async () => {
-  const endPoint = "http://localhost:3000/api/allproducts";
-  const products = await getData(endPoint);
-  const product = products.allData;
-  const filterData = product.filter((item) => item.category === "Bottle");
-
-  return filterData;
-};
+export const getManSneaker = () => getFilteredData("Men's Sneaker");
+export const getMenPant = () => getFilteredData("Men's Pants");
+export const getManBoot = () => getFilteredData("Men's Boot");
+export const getManBag = () => getFilteredData("Bag");
+export const getManCap = () => getFilteredData("Cap");
+export const getManEarPhone = () => getFilteredData("Earphones");
+export const getBottle = () => getFilteredData("Bottle");
