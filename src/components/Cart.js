e895212d68c07b3315dc38c76fce03240e0f274c
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-09-18 00:13:57 +0600

    solve

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -43,7 +43,7 @@ const Cart = () => {
   );
   const handleCheakOut = async () =>{
     const stripe = await stripePromise;
-    const response = await fetch("http://localhost:3000/api/checkout", {
+    const response = await fetch("https://eid-shopping.vercel.app/api/checkout", {
       method:"POST",
       headers:{
         "Content-type" : "application/json"
